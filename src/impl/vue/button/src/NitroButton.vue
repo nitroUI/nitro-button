59926dcedc265ff99d4962d39a1202bc4ee16a8a
Michael Carney <mcarney@gestalts-mbp.na.webmd.net>
2017-03-02 11:10:02 -0600

    nitro button simple structure

---
+++ src/impl/vue/button/src/NitroButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button>{{text}}</button>
+</template>
+
+<script>
+export default {
+  props: ['text'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+nitro-button {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
